c070063a5afcea7ae39174abd23fc9ad27692d3c
Nizobe <oshasnan@yahoo.com>
2017-08-30 16:54:55 -0400

    suuuuuure

--- CLI_Params/CLI-Params/Program.cs
+++ CLI_Params/CLI-Params/Program.cs
@@ -4,12 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//taken from
+//https://msdn.microsoft.com/en-us/library/aa288457(v=vs.71).aspx
+
 namespace CLI_Params
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Number of command line parameters: {0}", args.Length);
+
+            foreach(string str in args)
+            {
+                Console.WriteLine(str);
+            }
+
+            Console.WriteLine("END OF PARAMETERS");
+
+            Console.ReadKey();
         }
     }
 }
