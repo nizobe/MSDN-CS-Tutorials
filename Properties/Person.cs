6bb899dbcb7694325dfcb81d2fb6d92acffe650e
Nizobe <oshasnan@yahoo.com>
2017-08-30 17:50:19 -0400

    Added name prop
    Added property for Name and renamed the class to Person

---
+++ Properties/Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//take from:
+//https://msdn.microsoft.com/en-us/library/aa288470(v=vs.71).aspx
+
+namespace Properties
+{
+    class Person
+    {
+        private string myName = "N/A";
+        private int myAge = 0;
+
+        public string Name
+        {
+            get
+            {
+                return myName;
+            }
+
+            set
+            {
+                myName = value;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
