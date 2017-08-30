5926f1b32ef3a242baa1846f026a04ce80fb11b7
Nizobe <oshasnan@yahoo.com>
2017-08-30 13:52:37 -0400

    print arguments

--- CLI_Params/CLI-Params/Program.cs
+++ CLI_Params/CLI-Params/Program.cs
@@ -4,12 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//taken from
+//https://msdn.microsoft.com/en-us/library/aa288457(v=vs.71).aspx
+
 namespace CLI_Params
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Number of command line parameters: {0}", args.Length);
+
+            for(int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine("Arg[{0}] = [{1}]", i, args[i]);
+            }
+
+            Console.WriteLine("END OF PARAMETERS");
+
+            Console.ReadKey();
         }
     }
 }
